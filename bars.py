96d3cc6bc56cb8cb80fa54efc46d75aabcd5e7b3
Paul Vergeev <paulvergeev@gmail.com>
2017-02-16 07:17:49 +0300

    Change get_cli_args to get_argument_parser

--- bars.py
+++ bars.py
@@ -36,17 +36,17 @@ def print_bar(bar):
     print('Телефон: ' + bar['PublicPhone'][0]['PublicPhone'])
 
 
-def get_cli_args():
+def get_argument_parser():
     parser = ArgumentParser()
     parser.add_argument('what_to_look_for', type=str, 
             help='говорит скрипту, что нужно найти: ' +\
                  'biggest, smallest или closest')
     parser.add_argument('json_filepath', help='файл с данными о барах')
-    return parser.parse_args()
+    return parser
 
 
 if __name__ == '__main__':
-    args = get_cli_args()
+    args = get_argument_parser().parse_args()
 
     if not exists(args.json_filepath):
         exit('Файл с барами не найден.')
